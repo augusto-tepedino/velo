5f51154559e7d33f732fe534ccc73e18b4cdd25f
Augusto Tepedino <augusto.tepedino@gmail.com>
2026-01-30 04:38:23 -0300

    iniciando automação de teste de consulta de pedido aprovado

---
+++ playwright/e2e/pedidos.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+test('Deve consultar um pedido aprovado', async ({ page }) => {
+    const numeroDoPedido = 'VLO-0K831N';
+    await page.goto('http://localhost:5173/');
+    await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+    await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+
+    await expect(page.getByRole('heading', { name: 'Consultar Pedido' })).toBeVisible();
+    await page.getByTestId('search-order-id').fill(numeroDoPedido);
+    await page.getByTestId('search-order-button').click();
+
+    await expect(page.getByTestId('order-result-id')).toBeVisible();
+    await expect(page.getByTestId('order-result-id')).toContainText(numeroDoPedido);
+
+    await expect(page.getByTestId('order-result-status')).toBeVisible();
+    await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
+});
