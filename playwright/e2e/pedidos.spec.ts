5e60e94edf6224f7caf5e80a07cb06d23401aec7
Augusto Tepedino <augusto.tepedino@gmail.com>
2026-02-01 06:58:21 -0300

    Desafio sem testId

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -5,7 +5,6 @@ import { test, expect } from '@playwright/test';
 test('Deve consultar um pedido aprovado', async ({ page }) => {
     // Arrange
     const numeroDoPedido = 'VLO-0K831N';
-    const locatorNumeroDoPedido = page.getByText('Pedido', { exact: true }).locator('//../p[text()="${numeroDoPedido}"]');
     await page.goto('');
     await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
     await page.getByRole('link', { name: 'Consultar Pedido' }).click();
@@ -23,20 +22,26 @@ test('Deve consultar um pedido aprovado', async ({ page }) => {
 });
 
 test('Deve consultar um pedido aprovado - desafio', async ({ page }) => {
+
     // Arrange
-    const numeroDoPedido = 'VLO-0K831N';
+    const idPedido = 'VLO-0K831N';
+    const statusEsperado = 'APROVADO';
+    const numeroPedido = page.getByText('Pedido').locator(`//../p[text()="${idPedido}"]`);
+    const statusPedido = page.locator('//div[starts-with(@class, "flex items-center gap-2")]');
+
     await page.goto('');
     await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
     await page.getByRole('link', { name: 'Consultar Pedido' }).click();
     await expect(page.getByRole('heading', { name: 'Consultar Pedido' })).toBeVisible();
 
     // Act 
-    await page.getByLabel('Número do Pedido').fill(numeroDoPedido);
+    await page.getByLabel('Número do Pedido').fill(idPedido);
     await page.getByRole('button', { name: 'Buscar Pedido' }).click();
 
     //assert
-    await expect(page.getByTestId('order-result-id')).toBeVisible();
-    await expect(page.getByTestId('order-result-id')).toContainText(numeroDoPedido);
-    await expect(page.getByTestId('order-result-status')).toBeVisible();
-    await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
+    await expect(numeroPedido).toBeVisible();
+    await expect(numeroPedido).toContainText(idPedido);
+
+    await expect(statusPedido).toBeVisible();
+    await expect(statusPedido).toContainText(statusEsperado);
 });
